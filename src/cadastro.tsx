61b4fafb113e274326027d2a00dba1dbdb11b497
Yuri Monteiro <sfernanda1@outlook.com>
2023-11-29 15:41:13 -0300

    feature: tela de cadastro

---
+++ src/cadastro.tsx
@@ -0,0 +1,57 @@
+import { VStack, Image, Text, Box, Link, Checkbox, ScrollView } from 'native-base'
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Logo from './assets/Logo.png'
+import { Botao } from './componentes/botao';
+import { EntradaDeTexto } from './componentes/entradaDeTexto';
+import { Titulo } from './componentes/Titulo';
+import { secoes } from './utils/CadastroEntradaTexto';
+
+export default function Login() {
+  const [numSecao, setNumSecao] = useState(0);
+  
+
+  function avancarSecao(){
+    if(numSecao < secoes.length - 1){
+      setNumSecao(numSecao+1)
+    }
+  }
+
+  function voltarSecao(){
+    if(numSecao > 0){
+      setNumSecao(numSecao - 1)
+    }
+  }
+
+  return (
+    //O ScrollView permite a rolagem da tela o VStack não
+    <ScrollView flex={1} p={5}>
+      <Image source={Logo} alt="Logo Voll" alignSelf="center" />
+
+      <Titulo>
+        {secoes[numSecao].titulo}
+      </Titulo>
+      <Box>
+        {
+          secoes[numSecao]?.entradaTexto?.map(entrada => {
+            return <EntradaDeTexto label={entrada.label} placeholder={entrada.placeholder} key={entrada.id} />
+          })
+        }
+      </Box>
+      <Box>
+        <Text color="blue.800" fontWeight="bold" fontSize="md" mt="2" mb={2}>
+          Selecione o plano:
+        </Text>
+        {
+          secoes[numSecao].checkbox.map(checkbox => {
+            return <Checkbox key={checkbox.id} value={checkbox.value}>
+              {checkbox.value}
+            </Checkbox>
+          })
+        }
+      </Box>
+      {numSecao > 0 && <Botao onPress={() => voltarSecao()} bgColor="gray.400">Voltar</Botao>}
+      <Botao onPress={() => avancarSecao()} mt={4} mb={20}>Avançar</Botao>
+    </ScrollView>
+  );
+}
