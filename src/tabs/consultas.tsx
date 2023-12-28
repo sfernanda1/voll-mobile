355725ef46fc68cf284f82a57b86967d1f477992
Yuri Monteiro <sfernanda1@outlook.com>
2023-12-28 11:13:26 -0300

    feature: adicionando telas de exploracao, consultas, perfil e  home

--- src/tabs/consultas.tsx
+++ src/tabs/consultas.tsx
@@ -1,9 +1,37 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, Divider, ScrollView } from "native-base";
+import { Botao } from "../componentes/botao";
+import { CardConsulta } from "../componentes/cardConsulta";
+import { Titulo } from "../componentes/Titulo";
 
 export default function Consultas(){
     return(
-        <VStack>
-            <Text>Consultas</Text>
-        </VStack>
+        <ScrollView p={5}>
+            <Text fontWeight="bold" color="blue.500" fontSize="2xl" alignSelf="center">Minhas consultas </Text>
+            <Botao mt={5} mb={5}>Agendar nova consulta</Botao>
+            <Text fontWeight="bold" color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Próximas consultas </Text>
+            <CardConsulta 
+                nome="Dra. Marina"
+                especialidade="Cardiologista"
+                foto="https://avatars.githubusercontent.com/u/90799993?v=4"
+                data="12/12/2023"
+                foiAgendado
+            />
+            <Divider mt={5} />
+            <Text fontWeight="bold" color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Consultas Passadas </Text>
+            <CardConsulta 
+                nome="Dra. Marina"
+                especialidade="Cardiologista"
+                foto="https://avatars.githubusercontent.com/u/90799993?v=4"
+                data="12/12/2023"
+                foiAtendido
+            />
+             <CardConsulta 
+                nome="Dra. Marina"
+                especialidade="Cardiologista"
+                foto="https://avatars.githubusercontent.com/u/90799993?v=4"
+                data="12/12/2023"
+                foiAtendido
+            />
+        </ScrollView>
     )
 }
--- src/tabs/perfil.tsx
+++ src/tabs/perfil.tsx
@@ -1,9 +1,21 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, ScrollView, Avatar, Divider } from "native-base";
+import { Titulo } from "../componentes/Titulo";
 
 export default function Perfil(){
     return(
-        <VStack>
-            <Text>Perfil</Text>
+       <ScrollView flex={1}>
+        <VStack flex={1} alignItems="center" p={5}>
+            <Titulo color="blue.500">Meu Perfil</Titulo>
+            <Avatar size="xl" source={{uri: "https://avatars.githubusercontent.com/u/90799993?v=4"}} mt={5} />
+            <Titulo color="blue.500">Informações pessoais</Titulo>
+            <Titulo fontSize="lg" mb={1}>Fernanda Santos</Titulo>
+            <Text>01/01/2001</Text>
+            <Text>Recife</Text>
+            <Divider mt={5}/>
+            <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
+            <Text>Bronquite</Text>
+            <Text>Sinusite</Text>
         </VStack>
+       </ScrollView>
     )
 }
